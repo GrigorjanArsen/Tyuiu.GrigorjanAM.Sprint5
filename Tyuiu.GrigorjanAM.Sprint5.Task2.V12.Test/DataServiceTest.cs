b56f1dd85a6c5e908953fdc31a0dd7a9a02c76cb
Tyuiu.GrigorjanAM@outlook.com <djuravlik323@gmail.com>
2023-11-18 13:06:10 +0500

    Task2

---
+++ Tyuiu.GrigorjanAM.Sprint5.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.GrigorjanAM.Sprint5.Task2.V12.Lib;
+
+namespace Tyuiu.GrigorjanAM.Sprint5.Task2.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSTFTD()
+        {
+            string path = @"D:\Users\djura\source\repos\Tyuiu.GrigorjanAM.Sprint5\Tyuiu.GrigorjanAM.Sprint5.Task2.V12\bin\Debug\OutPutFileTask2.csv";
+            FileInfo fi = new FileInfo(path);
+            bool fileExists = fi.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+
+        }
+    }
+}
---
+++ Tyuiu.GrigorjanAM.Sprint5.Task2.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GrigorjanAM.Sprint5.Task2.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GrigorjanAM.Sprint5.Task2.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a058628f-3dc6-4ab3-9496-2123fb049765")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
